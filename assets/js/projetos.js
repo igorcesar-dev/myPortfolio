279f21c7cb1d0eba2fba2586088f14db2a40ccd0
Igor César <igorcesar.dev@gmail.com>
2022-11-16 23:38:47 -0300

    update_project

---
+++ assets/js/projetos.js
@@ -0,0 +1,50 @@
+const itens = [
+    {
+        nome: "Plataforma de Perguntas",
+        descricao: "Este projeto é uma plataforma de perguntas e respostas que o usuário pode realizar no site. O projeto foi elaborado no curso Formação node.js na Udemy.",
+        link: "https://github.com/igorcesar-dev/plataforma-de-perguntas",
+        id: "One"
+    },
+    {
+        nome: "Supermercado",
+        descricao: "Este projeto foi solicitado por um de meus professores na faculdade, no início, ele foi criado na linguagem Java, porém eu recriei ele em JavaScript.É um sistema de compras de produtos.",
+        link: "https://github.com/igorcesar-dev/supermarket",
+        id: "Two"
+    },
+    {
+        nome: "Helicopter Game",
+        descricao: "Este projeto foi elaborado na bootcamp Take Blip Web Developer, pertecente à platoforma da Digital Innovation One.",
+        link: "https://github.com/igorcesar-dev/helicopter-game",
+        id: "Three"
+    },
+    {
+        nome: "Menu de restaurante",
+        descricao: "Este projeto é uma página de menu de restaurante, que filtra os diferentes menus de comida disponíveis. Foi possível atribuir aprendizados sobre funções de ordem superior, como map, reduce e filter.",
+        link: "https://github.com/igorcesar-dev/restaurantMenu",
+        id: "Four"
+    },
+    {
+        nome: "Urna Eletrônica",
+        descricao: "Criado com HTML, CSS e JavaScript, este projeto se trata de uma urna eletrônica onde o usuário pode realizar a sua votação de acordo com a escolha para eleição.",
+        link: "https://github.com/igorcesar-dev/urnaEletronica",
+        id: "Five"
+    },
+    {
+        nome: "Carrossel de Revisões",
+        descricao: "Este projeto foi criado com JavaScript, é um carrossel de classificação com um botão que gera classificações aleatórias.Este é um bom recurso para ter em um site de comércio eletrônico para exibir avaliações de clientes.",
+        link: "https://github.com/igorcesar-dev/reviewCarousel",
+        id: "Six"
+    },
+    {
+        nome: "Jogo da Velha",
+        descricao: "Criado com HTML, CSS e JavaScript, este projeto se trata de uma urna eletrônica onde o usuário pode realizar a sua votação de acordo com a escolha para eleição.",
+        link: "https://github.com/igorcesar-dev/JogoDaVelha",
+        id: "Seven"
+    },
+    {
+        nome: "Quiz JavaScript",
+        descricao: "Este projeto foi realizado durante um curso de JavaScript com Node.JS na plataforma da Udemy. Utilizei HTML, CSS e JavaScript no desenvolvimento do projeto.",
+        link: "https://github.com/igorcesar-dev/quiz_game",
+        id: "Eight"
+    },
+]
---
+++ assets/js/script.js
@@ -0,0 +1,153 @@
+function iniciarProjetos() {
+    var containerProjetos = document.getElementById('projetos');
+    itens.map((valor) => {
+        containerProjetos.innerHTML += `
+<div class="box">
+        <div class="accordion-item ">
+        <h2 class="accordion-header" id="flush-heading`+valor.id+`">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+                data-bs-target="#flush-collapse`+valor.id+`" aria-expanded="false"
+                aria-controls="flush-collapse`+valor.id+`">
+                <h4 class="grid-title">`+ valor.nome +`</h4>
+            </button>
+        </h2>
+        <div id="flush-collapse`+valor.id+`" class="accordion-collapse collapse"
+            aria-labelledby="flush-heading`+valor.id+`" data-bs-parent="#accordionFlushExample">
+            <div class="accordion-body">
+    
+                <p class="grid-description">`+ valor.descricao +`</p>
+    
+                <a class="button" target="_blank"
+                    href="`+valor.link+`"><span>Acessar</span></a>
+            </div>
+        </div>
+    </div>
+</div>
+            `;
+    })
+}
+iniciarProjetos();
+
+
+             
+
+
+
+{/* <div class="accordion-item  ">
+    <h2 class="accordion-header" id="flush-headingTwo">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseTwo" aria-expanded="false"
+            aria-controls="flush-collapseTwo">
+            <h4 class="grid-title">Supermercado</h4>
+        </button>
+    </h2>
+    <div id="flush-collapseTwo" class="accordion-collapse collapse"
+        aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+
+            <p class="grid-description">Este projeto foi solicitado por um de meus professores na
+                faculdade, no
+                início, ele foi criado na linguagem Java, porém eu recriei ele em JavaScript. É um
+                sistema de
+                compras de produtos.</p>
+
+            <a class="button" target="_blank"
+                href="https://github.com/igorcesar-dev/supermarket"><span>Acessar</span></a>
+
+        </div>
+    </div>
+</div>
+
+<div class="accordion-item ">
+    <h2 class="accordion-header" id="flush-headingThree">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseThree" aria-expanded="false"
+            aria-controls="flush-collapseThree">
+            <h4 class="grid-title">Helicopter Game</h4>
+        </button>
+    </h2>
+    <div id="flush-collapseThree" class="accordion-collapse collapse"
+        aria-labelledby="flush-headingThree" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <p class="grid-description">Este projeto foi elaborado na bootcamp Take Blip Web
+                Developer,
+                pertecente à platoforma da Digital Innovation One.</p>
+
+            <a class="button" target="_blank"
+                href="https://github.com/igorcesar-dev/helicopter-game"><span>Acessar</span></a>
+
+        </div>
+    </div>
+</div>
+
+<div class="accordion-item ">
+    <h2 class="accordion-header" id="flush-headingFour">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseFour" aria-expanded="false"
+            aria-controls="flush-collapseFour">
+            <h4 class="grid-title">Menu de restaurante</h4>
+        </button>
+    </h2>
+    <div id="flush-collapseFour" class="accordion-collapse collapse"
+        aria-labelledby="flush-headingFour" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <p class="grid-description">Este projeto é uma página de menu de restaurante, que filtra
+                os
+                diferentes menus de comida disponíveis. Foi possível atribuir aprendizados sobre
+                funções
+                de
+                ordem superior, como map, reduce e filter.</p>
+
+            <a class="button" target="_blank"
+                href="https://github.com/igorcesar-dev/restaurantMenu"><span>Acessar</span></a>
+
+        </div>
+    </div>
+</div>
+
+<div class="accordion-item ">
+    <h2 class="accordion-header" id="flush-headingFive">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseFive" aria-expanded="false"
+            aria-controls="flush-collapseFive">
+            <h4 class="grid-title">Urna Eletrônica</h4>
+        </button>
+    </h2>
+    <div id="flush-collapseFive" class="accordion-collapse collapse"
+        aria-labelledby="flush-headingFive" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <p class="grid-description">Criado com HTML, CSS e JavaScript, este projeto se trata
+                de uma urna eletrônica onde o
+                usuário pode realizar a sua votação de acordo com a escolha para eleição.</p>
+
+            <a class="button" target="_blank"
+                href="https://github.com/igorcesar-dev/urnaEletronica"><span>Acessar</span></a>
+        </div>
+    </div>
+</div>
+
+<div class="accordion-item ">
+    <h2 class="accordion-header" id="flush-headingSix">
+        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
+            data-bs-target="#flush-collapseSix" aria-expanded="false"
+            aria-controls="flush-collapseSix">
+            <h4 class="grid-title">Menu de restaurante</h4>
+        </button>
+    </h2>
+    <div id="flush-collapseSix" class="accordion-collapse collapse"
+        aria-labelledby="flush-headingSix" data-bs-parent="#accordionFlushExample">
+        <div class="accordion-body">
+            <p class="grid-description">Este projeto é uma página de menu de restaurante, que filtra
+                os
+                diferentes menus de comida disponíveis. Foi possível atribuir aprendizados sobre
+                funções
+                de
+                ordem superior, como map, reduce e filter.</p>
+
+            <a class="button" target="_blank"
+                href="https://github.com/igorcesar-dev/restaurantMenu"><span>Acessar</span></a>
+
+        </div>
+    </div>
+</div>
+</div> */}

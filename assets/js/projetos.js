e4b5d8f31f2e3a7b7cefe3c01b18932b2bf2e746
igorcesar-dev <igorcesar.dev@gmail.com>
2023-03-20 12:28:44 -0300

    update_mar_23

--- assets/js/projetos.js
+++ assets/js/projetos.js
@@ -1,50 +1,56 @@
 const itens = [
     {
+        nome: "API de usuários",
+        descricao: "Está API é utilizada cada cadastrar, buscar, editar e/ou excluir usuários de um banco de dados.",
+        link: "https://github.com/igorcesar-dev/usersAPI",
+        id: "One"
+    },
+    {
+        nome: "Pokedéx com Vue.JS",
+        descricao: "Este projeto foi criado durante a realização do curso de Formação Node.JS na Udemy com o professor Victor Lima. Foi utilizada a API PokéAPI para obter os dados.",
+        link: "https://github.com/igorcesar-dev/pokedex",
+        id: "Two"
+    }, 
+    {
         nome: "Plataforma de Perguntas",
         descricao: "Este projeto é uma plataforma de perguntas e respostas que o usuário pode realizar no site. O projeto foi elaborado no curso Formação node.js na Udemy.",
         link: "https://github.com/igorcesar-dev/plataforma-de-perguntas",
-        id: "One"
+        id: "Three"
+    }, 
+    {
+        nome: "SysEduc",
+        descricao: "Este projeto foi desenvolvido durante a realizaçaõ das matérias de Análise e Modelagem de Sistemas e Processo de Desenvolvimento de Software. O projeto foi elaborado no curso superior de Sistemas de Informação no IFBA.",
+        link: "https://github.com/igorcesar-dev/syseduc",
+        id: "Four"
     },
     {
         nome: "Supermercado",
         descricao: "Este projeto foi solicitado por um de meus professores na faculdade, no início, ele foi criado na linguagem Java, porém eu recriei ele em JavaScript. É um sistema de compras de produtos.",
         link: "https://github.com/igorcesar-dev/supermarket",
-        id: "Two"
+        id: "Five"
     },
     {
         nome: "Helicopter Game",
         descricao: "Este projeto foi elaborado na bootcamp Take Blip Web Developer, pertecente à platoforma da Digital Innovation One.",
         link: "https://github.com/igorcesar-dev/helicopter-game",
-        id: "Three"
-    },
-    {
-        nome: "Menu de restaurante",
-        descricao: "Este projeto é uma página de menu de restaurante, que filtra os diferentes menus de comida disponíveis. Foi possível atribuir aprendizados sobre funções de ordem superior, como map, reduce e filter.",
-        link: "https://github.com/igorcesar-dev/restaurantMenu",
-        id: "Four"
+        id: "Six"
     },
     {
         nome: "Urna Eletrônica",
         descricao: "Criado com HTML, CSS e JavaScript, este projeto se trata de uma urna eletrônica onde o usuário pode realizar a sua votação de acordo com a escolha para eleição.",
         link: "https://github.com/igorcesar-dev/urnaEletronica",
-        id: "Five"
-    },
-    {
-        nome: "Carrossel de Revisões",
-        descricao: "Este projeto foi criado com JavaScript, é um carrossel de classificação com um botão que gera classificações aleatórias.Este é um bom recurso para ter em um site de comércio eletrônico para exibir avaliações de clientes.",
-        link: "https://github.com/igorcesar-dev/reviewCarousel",
-        id: "Six"
+        id: "Seven"
     },
     {
         nome: "Jogo da Velha",
         descricao: "Este projeto é um jogo da velha, foi criado com HTML, CSS e JavaScript. O projeto foi elaborado no curso Formação node.js na Udemy.",
         link: "https://github.com/igorcesar-dev/JogoDaVelha",
-        id: "Seven"
+        id: "Eight"
     },
     {
         nome: "Quiz JavaScript",
         descricao: "Este projeto foi realizado durante um curso de JavaScript com Node.JS na plataforma da Udemy. Utilizei HTML, CSS e JavaScript no desenvolvimento do projeto.",
         link: "https://github.com/igorcesar-dev/quiz_game",
-        id: "Eight"
+        id: "Nine"
     },
 ]
